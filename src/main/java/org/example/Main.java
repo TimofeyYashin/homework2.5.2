4542b3cdcb93bb6ff7f8b474c2d86c37bd661366
г <ashin.timoti2018@gmail.com>
2023-11-05 22:30:48 +0300

    homework2.5.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1,5, 1, 2,7, 3, 4, 4, 5,5, 6,9,7));
+        printOddNumbers(nums);
+        printEvenNumbers(nums);
+        List<String> words = new ArrayList<>(List.of("abs","abc","abb","add","ads","asd","ads"));
+        printWords(words);
+        printDoubles(words);
+    }
+
+    public static void printOddNumbers(List<Integer> nums) {
+        System.out.println("Задача 1");
+        for (Integer num : nums)
+            if (num % 2 != 0)
+                System.out.println(num);
+    }
+    public  static void printEvenNumbers (List<Integer> nums) {
+        System.out.println("Задача 2");
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for(Integer num:nums){
+            if (num%2==0 && num != prevNum) {
+                System.out.println(num);
+                prevNum=num;
+            }
+        }
+    }
+    public static void printWords (List<String> words) {
+        System.out.println("Задача 3");
+       Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
+    }
+    public static void printDoubles (List<String> words) {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(words.size() - uniqueWords.size());
     }
 }
+
