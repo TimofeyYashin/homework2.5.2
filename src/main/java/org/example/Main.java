58ba514591d80a0e8a98943bfeba6b56ca0dc8cd
г <ashin.timoti2018@gmail.com>
2023-11-05 22:31:21 +0300

    homework2.5.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1,5, 1, 2,7, 3, 4, 4, 5,5, 6,9,7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 5, 1, 2, 7, 3, 4, 4, 5, 5, 6, 9, 7));
         printOddNumbers(nums);
         printEvenNumbers(nums);
-        List<String> words = new ArrayList<>(List.of("abs","abc","abb","add","ads","asd","ads"));
+        List<String> words = new ArrayList<>(List.of("abs", "abc", "abb", "add", "ads", "asd", "ads"));
         printWords(words);
         printDoubles(words);
     }
@@ -18,23 +18,26 @@ public class Main {
             if (num % 2 != 0)
                 System.out.println(num);
     }
-    public  static void printEvenNumbers (List<Integer> nums) {
+
+    public static void printEvenNumbers(List<Integer> nums) {
         System.out.println("Задача 2");
         Collections.sort(nums);
         int prevNum = Integer.MIN_VALUE;
-        for(Integer num:nums){
-            if (num%2==0 && num != prevNum) {
+        for (Integer num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
                 System.out.println(num);
-                prevNum=num;
+                prevNum = num;
             }
         }
     }
-    public static void printWords (List<String> words) {
+
+    public static void printWords(List<String> words) {
         System.out.println("Задача 3");
-       Set<String> uniqueWords = new HashSet<>(words);
+        Set<String> uniqueWords = new HashSet<>(words);
         System.out.println(uniqueWords);
     }
-    public static void printDoubles (List<String> words) {
+
+    public static void printDoubles(List<String> words) {
         Set<String> uniqueWords = new HashSet<>(words);
         System.out.println(words.size() - uniqueWords.size());
     }
